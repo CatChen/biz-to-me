6e609f19cd9eb65594797f350abd260dc504db55
Cat Chen <cathsfz@gmail.com>
2011-07-20 23:26:10 +0800

    fixed logging bug

--- web.js
+++ web.js
@@ -1,15 +1,18 @@
 const http = require('http');
 
 var server = http.createServer(function(request, response) {
-    var host = request.headers['host'].replace(/catchen\.biz$/, 'catchen.me');
+    var host = request.headers['host'];
     var url = request.url;
+    
     console.log('request from: http://' + host + url);
+    host = host.replace(/catchen\.biz$/, 'catchen.me');
     if (host == request.headers['host']) {
         host = 'catchen.me';
         url = '/'
     }
     var location = 'http://' + host + url;
     console.log('redirect to: ' + location);
+    
     response.writeHead(302, {
         Location: location
     });
