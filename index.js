e66282b57690dc8724d01a7f30ad8142b8170a77
Cat Chen <catchen@catchen.me>
2019-09-20 12:33:08 -0700

    Installed and applied Prettier

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const http = require('http');
 
 var server = http.createServer(function(request, response) {
-    var protocol = request.headers['x-forwarded-proto'];
-    var host = request.headers['host'];
-    var url = request.url;
+  var protocol = request.headers['x-forwarded-proto'];
+  var host = request.headers['host'];
+  var url = request.url;
 
-    console.log('request from: ' + protocol + '://' + host + url);
-    host = host.replace(/catchen\.biz$/, 'catchen.me');
-    if (host == request.headers['host']) {
-        host = 'catchen.me';
-        url = '/'
-    }
-    var location = protocol + '://' + host + url;
-    console.log('user-agent: ' + request.headers['user-agent']);
-    console.log('redirect to: ' + location);
+  console.log('request from: ' + protocol + '://' + host + url);
+  host = host.replace(/catchen\.biz$/, 'catchen.me');
+  if (host == request.headers['host']) {
+    host = 'catchen.me';
+    url = '/';
+  }
+  var location = protocol + '://' + host + url;
+  console.log('user-agent: ' + request.headers['user-agent']);
+  console.log('redirect to: ' + location);
 
-    response.writeHead(301, {
-        Location: location
-    });
-    response.write('Permanently moved to <a href="' + location + '">' + location + '</a>.');
-    response.end();
+  response.writeHead(301, {
+    Location: location,
+  });
+  response.write(
+    'Permanently moved to <a href="' + location + '">' + location + '</a>.',
+  );
+  response.end();
 });
 
 var port = process.env.PORT || 3000;
-server.listen(port, function(){
-    console.log("Listening on " + port);
+server.listen(port, function() {
+  console.log('Listening on ' + port);
 });
